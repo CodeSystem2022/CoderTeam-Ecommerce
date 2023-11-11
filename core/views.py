90a04bbf1c11e72e95d0219f5f5939d86c2a82ee
Pepe <natanjabie09@gmail.com>
2023-11-11 19:30:46 -0300

    add images categories and items

--- core/views.py
+++ core/views.py
@@ -1,26 +1,42 @@
 from django.shortcuts import render, redirect
 
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout 
 from item.models import Category, Item
 
-from django.http import HttpResponse
 from .forms import SignupForm
+
+from PIL import Image
+
+import os
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
+    
+    for item in items:
+        
+        path = os.getcwd() + item.image.url
+        with Image.open(path) as img:
+            # Resize the image to the desired dimensions
+            resized_img = img.resize((5536, 4160))
+
+            # Overwrite the original image with the resized image
+            resized_img.save(path)
+
     return render(request, 'core/index.html', {
         'categories': categories,
         'items': items,
     })
+
 def contact(request):
     return render(request, 'core/contact.html')
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
+
         if form.is_valid():
             form.save()
+
             return redirect('/login/')
     else:
         form = SignupForm()
@@ -28,8 +44,3 @@ def signup(request):
     return render(request, 'core/signup.html', {
         'form': form
     })
-
-@login_required
-def logout_user(request):
-    logout(request)
-    return redirect('dashboard:index')
